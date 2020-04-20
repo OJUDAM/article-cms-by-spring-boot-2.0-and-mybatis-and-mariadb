486ffa59b3dd13f41a3dca2b7bfa223e5d24c4ac
OHJUDAM <dhwnkea@naver.com>
2020-04-20 21:27:28 +0900

    프로젝트 생성 및 기본적인 세팅 완료

---
+++ src/test/java/com/example/starter/StarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class StarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
