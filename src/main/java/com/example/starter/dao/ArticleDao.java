0ccdf145e7f76e44080b80677022115e3be156df
OHJUDAM <dhwnkea@naver.com>
2020-04-21 16:10:35 +0900

    ArticledaoImpl을 제거하고 마이바티스가 대신 구현하도록 처리(마이바티스는 데이터를 DB에서 가져옴, 즉 이제 가짜 데이터는 끝)

--- src/main/java/com/example/starter/dao/ArticleDao.java
+++ src/main/java/com/example/starter/dao/ArticleDao.java
@@ -1,5 +1,12 @@
 package com.example.starter.dao;
 
-public interface ArticleDao {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.starter.dto.Article;
+
+@Mapper //이렇게 해주면 ArticleDao의 구현체를 마이바티스가 대신 구현해준다.
+public interface ArticleDao {
+	public List<Article> getList();
 }
--- src/main/java/com/example/starter/service/ArticleServiceImpl.java
+++ src/main/java/com/example/starter/service/ArticleServiceImpl.java
@@ -17,17 +17,20 @@ public class ArticleServiceImpl implements ArticleService {
 	//ArticleService는 데이터 관련해서는 모두 Dao에게 위힘
 	
 	public List<Article> getList(){
-		//중간확인용
-		Article article1 = new Article(1, "2019-08-20 12:12:12","제목1","내용1");
-		Article article2 = new Article(2, "2019-08-20 12:12:13","제목2","내용2");
-		Article article3 = new Article(3, "2019-08-20 12:12:14","제목3","내용3");
 		
-		List<Article> list = new ArrayList<>();
 		
-		list.add(article3);
-		list.add(article2);
-		list.add(article1);
+		return articleDao.getList();
 		
-		return list;
+		/*
+		 * //중간확인용 Article article1 = new Article(1, "2019-08-20 12:12:12","제목1","내용1");
+		 * Article article2 = new Article(2, "2019-08-20 12:12:13","제목2","내용2"); Article
+		 * article3 = new Article(3, "2019-08-20 12:12:14","제목3","내용3");
+		 * 
+		 * List<Article> list = new ArrayList<>();
+		 * 
+		 * list.add(article3); list.add(article2); list.add(article1);
+		 * 
+		 * return list;
+		 */
 	}
 }
